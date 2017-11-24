2db6034608e29871cc28e503a9d8a1947a1253bf
Johan Viklund <johan.viklund@nbis.se>
2017-11-24 15:52:28 +0000

    Add travis integration scripts

---
+++ scripts/travis-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xeuo pipefail
+
+nextflow run main.nf
+
+# Very simple check to make sure that we have some output
+outbytes=$(wc -c out/file.bam | awk '{print $1}')
+
+if [ $outbytes -lt 100 ]; then
+    exit 1
+fi
