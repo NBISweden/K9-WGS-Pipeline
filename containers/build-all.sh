6905bc2a299d8b2d793daa14765dde221658cb63
Johan Viklund <johan.viklund@nbis.se>
2017-12-04 17:13:15 +0000

    Updated container building

--- containers/build-all.sh
+++ containers/build-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for dir in {bwa,gatk,htslib,picard,samtools}; do
+## Needed for all, no need to download one thing in parallel
+docker pull conda/miniconda3:latest
+
+for dir in $(ls */Dockerfile | sed 's,/Dockerfile,,'); do
     # Not more than 4 jobs at the same time
     while [ $(jobs -p | wc -l) -ge 4 ]; do
         sleep 1
