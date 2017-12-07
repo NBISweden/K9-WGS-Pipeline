d0a921b8c568203dfa28ea8c3854fd65942c8c9c
Johan Viklund <johan.viklund@nbis.se>
2017-12-07 16:49:29 +0000

    Added docker to travis tests

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -xeuo pipefail
 
-nextflow run main.nf --verbose --fastqDir test-data/test-data-tiny
+if [ -z "$PROFILE" ]; then
+    echo "No profile specified"
+    exit 1
+fi
+
+nextflow run main.nf -profile $PROFILE --verbose --fastqDir test-data/test-data-tiny
 
 # Very simple check to make sure that we have some output
 outbytes=$(wc -c out/file.bam | awk '{print $1}')
