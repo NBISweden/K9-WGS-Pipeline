3e41d8e4fa8b1dfd300142deae439b334ec9a9bf
Johan Viklund <johan.viklund@nbis.se>
2017-12-08 13:36:05 +0000

    Install singularity even if we do the docker tests

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -7,15 +7,12 @@ chmod +x nextflow
 sudo mv nextflow /usr/local/bin/
 
 # Install Singularity
-if [[ "$PROFILE" = singularity ]]
-then
-    cd $HOME
-    wget https://github.com/singularityware/singularity/releases/download/$SGT_VER/singularity-$SGT_VER.tar.gz
-    tar xvf singularity-$SGT_VER.tar.gz
-    cd singularity-$SGT_VER
-    ./configure --prefix=/usr/local
-    make
-    sudo make install
-    cd ..
-    rm -rf singularity-$SGT_VER*
-fi
+cd $HOME
+wget https://github.com/singularityware/singularity/releases/download/$SGT_VER/singularity-$SGT_VER.tar.gz
+tar xvf singularity-$SGT_VER.tar.gz
+cd singularity-$SGT_VER
+./configure --prefix=/usr/local
+make
+sudo make install
+cd ..
+rm -rf singularity-$SGT_VER*
