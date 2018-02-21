ed3dc648fb8e6d08d44465091399a0d8c96cd74b
Johan Viklund <johan.viklund@nbis.se>
2018-02-21 12:34:56 +0000

    First stab at expanding travis testing

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -14,3 +14,17 @@ outbytes=$(wc -c out/file.bam | awk '{print $1}')
 if [ $outbytes -lt 100 ]; then
     exit 1
 fi
+
+nextflow run main.nf -profile $PROFILE \
+    --verbose \
+    --fastqDir test-data/test-data-small/ \
+    --reference test-data/test-data-small/reference_subset.fa \
+    --out out2 \
+    --known test-data/test-data-small/known_subset.bed
+
+# Very simple check to make sure that we have some output
+outbytes=$(wc -c out2/file.bam | awk '{print $1}')
+
+if [ $outbytes -lt 100 ]; then
+    exit 1
+fi
