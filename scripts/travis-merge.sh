258450b28c1bc5b3e4c69d67497f19444ebc2149
Johan Viklund <johan.viklund@nbis.se>
2018-03-17 22:15:11 +0100

    Correct automerge script

--- scripts/travis-merge.sh
+++ scripts/travis-merge.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -e
 
-# Only auto merge develop branch for now
-if [ "$TRAVIS_BRANCH" != "develop" ]; then
+# Only auto merge develop branch
+if [ "$TRAVIS_BRANCH" != "develop" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
     exit 0;
 fi
 
-export GIT_COMMITER_EMAIL='johan.viklund@nbis.se'
-export GIT_COMMITER_NAME='Johan Viklund'
+git config user.email "johan.viklund@nbis.se"
+git config user.name "Travis CI"
 
-git fetch
-git checkout master || exit
-git merge "$TRAVIS_COMMIT" || exit
-git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git HEAD:master
+git fetch --depth=50 origin refs/heads/master:master
+git checkout master
+git merge --no-ff "$TRAVIS_COMMIT"
+git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git master:refs/heads/master
