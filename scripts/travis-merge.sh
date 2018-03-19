94aa3d20ccc87c6e850c8e516a0299adbb6361cc
Johan Viklund <johan.viklund@nbis.se>
2018-03-19 08:04:09 +0000

    Updated merge script

--- scripts/travis-merge.sh
+++ scripts/travis-merge.sh
@@ -11,5 +11,5 @@ git config user.name "Travis CI"
 
 git fetch --depth=50 origin refs/heads/master:master
 git checkout master
-git merge --no-ff "$TRAVIS_COMMIT"
-git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git master:refs/heads/master
+git merge --ff-only "$TRAVIS_COMMIT"
+git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git master
