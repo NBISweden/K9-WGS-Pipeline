038394d2d96e1a64d4a05d9e1ade28739062d3d9
Johan Viklund <johan.viklund@nbis.se>
2018-03-22 13:10:59 +0000

    Renamed test data

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -10,8 +10,8 @@ nextflow run main.nf \
     -profile singularity \
     --verbose \
     --fastqDir  test-data/test-data-tiny \
-    --reference test-data/test-data-tiny/reference_chr38-1000000-1030000.fa \
-    --known     test-data/test-data-tiny/known_chr38-1000000-1030000.bed \
+    --reference test-data/test-data-tiny/reference.fa \
+    --known     test-data/test-data-tiny/known.bed \
     --out       out-tiny
 
 # Very simple check to make sure that we have some output
@@ -25,8 +25,8 @@ nextflow run main.nf \
     -profile singularity \
     --verbose \
     --fastqDir  test-data/test-data-small/ \
-    --reference test-data/test-data-small/reference_subset.fa \
-    --known     test-data/test-data-small/known_subset.bed \
+    --reference test-data/test-data-small/reference.fa \
+    --known     test-data/test-data-small/known.bed \
     --out       out-small
 
 # Very simple check to make sure that we have some output
